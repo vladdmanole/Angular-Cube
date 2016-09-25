22bac83d188bb8dae8cb243547d0606268339463
vladdmanole <vladdmanole@gmail.com>
2016-09-26 02:13:53 +0300

    saving events in construction

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 (function() {
 
-  var app = angular.module("CubeCalendar", []);
+    var app = angular.module("CubeCalendar");
 
   var MainController = function ($scope) {
+
+      $scope.count = 0;
+      $scope.saveEvent = function () {
+          var savedata = $scope.userTime + " " + $scope.userText + " " + $scope.userTitle;
+      };
+
+
+      $scope.saveEvent = function (event) {
+          localStorage("title" + count, $scope.userTitle);
+      };
+
+      $scope.getEvent = function () {
+          console.log(document.getElementById("result").innerHTML = localStorage.getItem("title" + $scope.count));
+      };
+
+      $scope.removeEvent = function () {
+          $scope.removeEvent()
+      };
+
       var xAngle = yAngle = 0;
       $scope.keyPress = function (keyCode) {
           switch (keyCode) {
@@ -25,7 +44,7 @@
                   break; // down
           }
           document.getElementById('cube').style.transform = "rotateX(" + xAngle + "deg) rotateY(" + yAngle + "deg)";
-      };
+      }
 
 
   };
