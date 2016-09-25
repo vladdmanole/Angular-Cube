237485f6289c91d96d3d0b72860fae84111a5bda
vladdmanole <vladdmanole@gmail.com>
2016-09-25 18:26:14 +0300

    overflow disabled. textarea is working on firefox finally

--- script.js
+++ script.js
@@ -9,15 +9,19 @@
 
               case 37:
                   yAngle += 90;
+
                   break; // left
               case 38:
                   xAngle -= 90;
+
                   break; // up
               case 39:
                   yAngle -= 90;
+
                   break; // right
               case 40:
                   xAngle += 90;
+
                   break; // down
           }
           document.getElementById('cube').style.transform = "rotateX(" + xAngle + "deg) rotateY(" + yAngle + "deg)";
