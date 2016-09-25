570276d057f9517c1efa863786c78175bebdb62e
vladdmanole <vladdmanole@gmail.com>
2016-09-25 15:00:08 +0300

    built basic forms for calendar app

--- script.js
+++ script.js
@@ -7,10 +7,18 @@
       $scope.keyPress = function (keyCode) {
           switch (keyCode) {
 
-              case 37: yAngle += 90; break;
-              case 38: xAngle -= 90; break;
-              case 39: yAngle -= 90; break;
-              case 40: xAngle += 90; break;
+              case 37:
+                  yAngle += 90;
+                  break; // left
+              case 38:
+                  xAngle -= 90;
+                  break; // up
+              case 39:
+                  yAngle -= 90;
+                  break; // right
+              case 40:
+                  xAngle += 90;
+                  break; // down
           }
           document.getElementById('cube').style.transform = "rotateX(" + xAngle + "deg) rotateY(" + yAngle + "deg)";
       };
